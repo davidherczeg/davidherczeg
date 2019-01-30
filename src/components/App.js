44972be32709d9c8ce126e66ed8e05e627d338ec
David Herczeg <david.herczeg@live.ca>
2019-01-30 13:13:16 -0700

    Setting up react and webpack boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome David Herczeg's personal website</h1>
+        <p>The site is currently under development and will be up soon!</p>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {}
+
+export default App;
---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(['public']),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          }
+        }
+      }
+    ]
+  }
+};
