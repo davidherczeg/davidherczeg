06b787bc18cc12f11dd685157727a3a2be461002
David Herczeg <david.herczeg@live.ca>
2020-05-10 18:33:54 -0600

    Configure and import Tailwind styles

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import '../styles/styles.css';
+
+export default ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
---
+++ pages/home.js
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <div className='w-full h-screen bg-gray-400'>
+      <h1>Home</h1>
+    </div>
+  );
+};
