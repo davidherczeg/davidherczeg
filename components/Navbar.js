2cd5db4752e0d257aeee1543fcb26c6ae5c20253
David Herczeg <david.herczeg@live.ca>
2020-05-10 21:59:27 -0600

    Develop responsive navbar outline

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,40 @@
-// portfolio instead of projects
-// blog??
-const links = ['About', 'Projects', 'Contact'];
+import Link from 'next/link';
+import Logo from '../assets/logo.svg';
+import Menu from '../assets/menu.svg';
 
-function Navbar() {
+const links = [
+  { href: '/', label: 'About' },
+  { href: '/', label: 'Blog' },
+  { href: '/', label: 'Portfolio' },
+  { href: '/', label: 'Contact' },
+];
+
+export default () => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <header className='flex justify-between items-center px-16 sm:px-32 py-4  bg-gray-500'>
+      <div>
+        <Link href='/'>
+          <a className='text-blue-500 no-underline'>
+            <Logo className='w-16' />
+          </a>
+        </Link>
+      </div>
+      <div className='sm:hidden'>
+        <button>
+          <Menu className='w-auto' />
+        </button>
+      </div>
+      <nav className='hidden sm:block'>
+        <ul className='flex items-center space-x-8'>
+          {links.map(({ href, label }) => (
+            <li key={`${href}${label}`}>
+              <Link href={href}>
+                <a className=''>{label}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
   );
-}
-
-export default Navbar;
+};
--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,9 @@
+import Navbar from '../components/Navbar';
+
 export default () => {
   return (
     <div className='w-full h-screen bg-gray-400'>
+      <Navbar />
       <h1>Home</h1>
     </div>
   );
