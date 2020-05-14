05c299f3b4b2e46c0d49e3fb96e352ca132e75be
David Herczeg <david.herczeg@live.ca>
2020-05-13 23:45:17 -0600

    Exclude node_modules from SVG build process

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports = {
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push({
       test: /\.svg/,
+      exclude: path.resolve(__dirname, 'node_modules'),
       use: ['@svgr/webpack'],
     });
 
