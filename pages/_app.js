212c516f2e1123c8c5b5f5fe2eb0b9a9cdd9e8bf
David Herczeg <david.herczeg@live.ca>
2021-05-19 15:36:49 -0600

    Display favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import Head from 'next/head';
 import '../styles/styles.css';
 
 export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel='icon' type='image/png' sizes='256x256' href='/favicon.png' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
