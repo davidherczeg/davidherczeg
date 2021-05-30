143c6d9b42e928253dcec7b518a4a776c0ab7e18
David Herczeg <david.herczeg@live.ca>
2021-05-29 18:55:03 -0600

    Update hero image for larger screens

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const Hero = props => {
   return (
     <div
-      className='flex flex-col justify-center w-full px-24 md:px-32 lg:px-16 xl:px-32 bg-hero space-y-2'
+      className='flex flex-col justify-center w-full px-24 md:px-32 lg:px-16 xl:px-32 bg-hero bg-center space-y-2'
       style={{ height: 500, marginTop: -68 }}
     >
       <h1 className='text-white text-4xl sm:text-5xl lg:text-6xl text-center lg:text-left'>
