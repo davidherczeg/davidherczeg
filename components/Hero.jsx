7e463341ca302fe429c1c3e653b0e72af55d632e
David Herczeg <david.herczeg@live.ca>
2021-05-29 19:18:10 -0600

    Make hero image responsive to smaller screens too

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,10 +6,10 @@ const Hero = props => {
       className='flex flex-col justify-center w-full px-24 md:px-32 lg:px-16 xl:px-32 bg-hero bg-center space-y-2'
       style={{ height: 500, marginTop: -68 }}
     >
-      <h1 className='text-white text-4xl sm:text-5xl lg:text-6xl text-center lg:text-left'>
+      <h1 className='text-white text-4xl lg:text-6xl'>
         Hello! I'm <span className='text-purple-500'>David</span>
       </h1>
-      <h1 className='text-white text-4xl sm:text-5xl lg:text-6xl text-center lg:text-left'>
+      <h1 className='text-white text-4xl lg:text-6xl'>
         A <span className='text-purple-500'>Software Developer</span>
       </h1>
     </div>
