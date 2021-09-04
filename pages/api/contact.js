c7b8e6942ec4b91f4c5eab9a010b9be8575542a2
David Herczeg <david.herczeg@live.ca>
2021-09-04 15:29:35 -0600

    SMTP setup for contact form

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,5 @@
 import nextConnect from 'next-connect';
+import nodemailer from 'nodemailer';
 
 const handler = nextConnect();
 
@@ -8,6 +9,32 @@ handler.post(async (req, res) => {
   if (!name || !email || !subject || !message) {
     return res.status(400).send('Missing fields');
   }
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  const info = await transporter.sendMail({
+    from: process.env.EMAIL,
+    to: process.env.EMAIL,
+    subject: `Contact Form: ${subject}`,
+    text: message,
+    html: `
+      <p>${message}</p>
+      <b>${name}</b>
+      </br>
+      <em>${email}</em>
+    `,
+  });
 
   res.status(201).json({ message: 'Success' });
 });
